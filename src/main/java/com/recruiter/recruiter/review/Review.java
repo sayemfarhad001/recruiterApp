c62f225332d9166b1439772fad32c56a5094de0e
Sayem <sayemfarhad001@gmail.com>
2024-07-03 15:26:49 -0400

    setup review complete

--- src/main/java/com/recruiter/recruiter/review/Review.java
+++ src/main/java/com/recruiter/recruiter/review/Review.java
@@ -1,5 +1,46 @@
 package com.recruiter.recruiter.review;
 
-public class Review {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String desctiption;
+    private double rating;
+
+
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDesctiption() {
+        return desctiption;
+    }
+    public void setDesctiption(String desctiption) {
+        this.desctiption = desctiption;
+    }
+    public double getRating() {
+        return rating;
+    }
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    
 }
--- src/main/java/com/recruiter/recruiter/review/ReviewController.java
+++ src/main/java/com/recruiter/recruiter/review/ReviewController.java
@@ -1,5 +1,19 @@
 package com.recruiter.recruiter.review;
 
-public class ReviewController {
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+
+@RestController
+@RequestMapping("/companies/{companyId}")
+public class ReviewController {
+    private ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    
 }
